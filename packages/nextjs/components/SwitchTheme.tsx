f17732344404eb3dc3928f56f52d69b4993c4e1d
Lulox <lulox.eth@protonmail.com>
2024-10-22 10:28:51 -0300

    Fixed switch theme not showing on mobile

--- packages/nextjs/components/SwitchTheme.tsx
+++ packages/nextjs/components/SwitchTheme.tsx
@@ -22,9 +22,10 @@ export const SwitchTheme = ({ className }: { className?: string }) => {
 
   return (
     <div
-      className={`flex space-x-2 h-8 items-center hover:bg-[#C9D8E5] dark:hover:bg-[#193048] justify-start w-full rounded-lg pl-3 text-sm ${className}`}
+      onClick={handleToggle}
+      className={`flex cursor-pointer space-x-2 h-8 items-center hover:bg-[#C9D8E5] dark:hover:bg-[#193048] justify-start w-full rounded-lg pl-3 text-sm ${className}`}
     >
-      <button onClick={handleToggle} className="swap swap-rotate flex flex-row gap-2 justify-start items-center">
+      <button className="swap swap-rotate flex flex-row gap-2 justify-start items-center">
         {isDarkMode ? <MoonIcon className="h-5 w-5" /> : <SunIcon className="h-5 w-5" />}
         <span>Switch theme</span>
       </button>
