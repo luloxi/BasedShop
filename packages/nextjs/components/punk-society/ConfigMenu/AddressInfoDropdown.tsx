bf30cc759d30b86c1a7b178c882ffc0a87ce2871
Lulox <lulox.eth@protonmail.com>
2024-10-22 10:26:26 -0300

    Fixed switch theme not showing on mobile

--- packages/nextjs/components/punk-society/ConfigMenu/AddressInfoDropdown.tsx
+++ packages/nextjs/components/punk-society/ConfigMenu/AddressInfoDropdown.tsx
@@ -73,7 +73,7 @@ export const AddressInfoDropdown = ({ blockExplorerAddressLink }: AddressInfoDro
               </button>
             </li>
           ) : null}
-          <div className="hidden lg:flex items-center justify-center ">
+          <div className="flex items-center justify-center ">
             <SwitchTheme />
           </div>
           <li className={selectingNetwork ? "hidden" : ""}>
